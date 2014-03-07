df6c5b2fd43ce9078860b10ab0a218075de3ec18
Martin Fietz <Martin.Fietz@googlemail.com>
2014-03-07 13:27:11 +0100

    New paremeter overwrite for convert() for future reference. Switched os.system() for subprocess module. stderr is now suppressed.

--- gif2mp4.py
+++ gif2mp4.py
@@ -2,26 +2,38 @@
 # -*- coding: utf-8 -*-
 
 import os
+import os.path
 import re
+import subprocess
 import sys
 
-def convert(file):
+devnull = open('/dev/null', 'w')
+
+def convert(file,overwrite=False,keep=True):
     if not file.endswith('.gif'):
         return
     base_name = file[:-4]
+    target = base_name + ".mp4"
+    if os.path.exists(target) and not overwrite:
+        return
     duration = get_duration(file)
     frame_count = get_frame_count(file)
+    if not frame_count:
+        return
     if not duration:
         frame_rate = 5
     else:
         frame_rate = frame_count/duration
-    cmd("ffmpeg -v quiet -r " + str(frame_rate) + " -i  " + file + " -crf 20 -tune film -preset veryslow -y -an " + base_name + ".mp4")
+    cmd("ffmpeg -v quiet -r " + str(frame_rate) + " -i  " + file + " -crf 20 -tune film -preset veryslow -y -an " + target)
+    if not keep:
+        os.system("rm " + file)
 
 def cmd(cmd):
     os.system(cmd)
 
 def get_duration(file):
-    output = os.popen("exiftool -Duration " + file).read()
+    args = [ "exiftool", "-Duration", file ]
+    output = subprocess.check_output(args, stderr=devnull)
     match = re.search(r'^Duration                        : (\d+.\d+) s\n$', output)
     if match:
         return float(match.group(1))
@@ -29,7 +41,8 @@ def get_duration(file):
         return None
 
 def get_frame_count(file):
-    output = os.popen("exiftool -FrameCount " + file).read()
+    args = [ "exiftool", "-FrameCount", file ]
+    output = subprocess.check_output(args, stderr=devnull)
     match = re.search(r'Frame Count                     : (\d+)\n$', output)
     if match:
         return int(match.group(1))
